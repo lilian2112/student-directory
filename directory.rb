288c1b238e07e49473059259f8e58086ded57e75
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-13 20:18:36 +0100

    you can now input cohort

--- directory.rb
+++ directory.rb
@@ -3,14 +3,25 @@ def input_students
   additional_info = [:hobbies, :country_of_birth, :height, :relationship_status]
   while true do
     new_hash = {}
-    puts "Please enter the names of a student"
-    puts "To finish, just hit return twice"
+    puts "Please enter the names of a student, to finish, just hit return twice"
     name = gets.chomp
       if name == ''
         break
       end
+      puts "Please enter the cohort of that student"
+      cohort = gets.chomp.to_s
+      if cohort == '' or nil
+        cohort = "September"
+      elsif ["january", "feburary", "march", "april", "may", "june", "july", "august", "september", "november", "december"].include? (cohort.downcase)
+      else
+        puts "Please enter a valid cohort, spell out the month, otherwise it will default to september"
+        cohort = gets.chomp
+        unless ["january", "feburary", "march", "april", "may", "june", "july", "august", "september", "november", "december"].include? (cohort.downcase)
+         cohort = "September"
+       end
+      end
         new_hash[:name] = name
-        new_hash[:cohort] = :september
+        new_hash[:cohort] = cohort.to_sym
         additional_info.each do |info|
           puts "Please enter #{info.to_s} for #{name}."
           information = gets.chomp
@@ -33,7 +44,7 @@ def print(students)
   students.each do |student|
   while index != n do
   student = students[index]
-     puts "#{student[:name]} (#{student[:cohort]} cohort), their hobby is #{student[:hobbies]}, they were born in #{student[:country_of_birth]}, and they are #{student[:heigth]}cm tall"
+     puts "#{student[:name].center(12)} (#{student[:cohort]} cohort), their hobby is #{student[:hobbies]}, they were born in #{student[:country_of_birth]}, and they are #{student[:heigth]}cm tall."
   index += 1
 end
 end
