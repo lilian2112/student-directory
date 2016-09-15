eb968248733ed65e0e650203ab076799316cf4a9
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-15 10:47:08 +0100

    we can now save data

--- directory.rb
+++ directory.rb
@@ -12,6 +12,8 @@ def process(selection )
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -22,6 +24,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of sudents.csv"
   puts "9. Exit"
 end
 
@@ -32,12 +35,11 @@ def show_students
 end
 
 
-
 def input_students
   additional_info = [:hobbies, :country_of_birth, :height]
   while true do
     new_hash = {}
-    puts "Please enter the names of a student, to finish, just hit return twice"
+    puts "Please enter the names of a student, to finish, just hit return again"
     name = gets.delete "\n"
       if name == ''
         break
@@ -46,11 +48,11 @@ def input_students
       cohort = gets.chomp.to_s
       if cohort == '' or nil
         cohort = "september"
-      elsif ["january", "feburary", "march", "april", "may", "june", "july", "august", "september", "november", "december"].include? (cohort.downcase)
+      elsif ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"].include? (cohort.downcase)
       else
         puts "Please enter a valid cohort, spell out the month, otherwise it will default to september"
         cohort = gets.chomp
-        unless ["january", "feburary", "march", "april", "may", "june", "july", "august", "september", "november", "december"].include? (cohort.downcase)
+        unless ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"].include? (cohort.downcase)
          cohort = "september"
        end
       end
@@ -66,6 +68,18 @@ def input_students
   @students
 end
 
+def save_students
+  #open file for writing
+  file = File.open("students.csv", "w")
+  # itterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 
 def print_header
   puts "The students of Villain Academy"
@@ -77,34 +91,19 @@ def print_by_cohort
   group_cohort.each do |cohort|
     @students.each do |student|
       if student[:cohort] == cohort
-        puts "#{student[:name].center(12)} (#{student[:cohort]} cohort), their hobby is #{student[:hobbies]}, they were born in #{student[:country_of_birth]}, and they are #{student[:heigth]}cm tall"
+        puts "#{student[:name].center(12)}, (#{student[:cohort]} cohort), their hobby is #{student[:hobbies]}, they were born in #{student[:country_of_birth]}, and they are #{student[:heigth]}cm tall"
 end
 end
 end
 end
 
-=begin
-this is an alternative sort method. but it only sorts cohorts alphabetically
-def print(students)
-  new_students = students.sort_by{|c| c[:cohort]}
-  index = 0
-  n = new_students.length
-  new_students.each do |student|
-  while index != n do
-  student = new_students[index]
-     puts "#{student[:name].center(12)} (#{student[:cohort]} cohort), their hobby is #{student[:hobbies]}, they were born in #{student[:country_of_birth]}, and they are #{student[:heigth]}cm tall."
-  index += 1
-end
-end
-end
-=end
 
 
 # finally, we print the total number of students
 def print_footer
   if @students.count == 0
     puts "We currently have no students"
-  elsif @students.count <= 2
+  elsif @students.count <= 1
    puts "Overall, we have #{@students.count} great student"
   else
    puts "Overall, we have #{@students.count} great students"
